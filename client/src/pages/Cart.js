4f59c41235eebb2638c905d6d913a5bcab82c6dd
tosimi adesoye <tosimiadesoye111@gmail.com>
2020-12-18 14:21:45 +0000

    started styling cart

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -1,20 +1,42 @@
 import { useState, useEffect } from "react";
 import CartService from "../services/product";
 import Spinner from "react-bootstrap/Spinner";
-import makeStyles from '@material-ui/core/styles/makeStyles'
-
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import { Paper } from "@material-ui/core";
 const useStyles = makeStyles((theme) => ({
   root: {
-    '& > *': {
-      margin: theme.spacing(1)
+    flexGrow: 1,
+    border: "1px solid black",
+    marginTop: theme.spacing(7),
+  },
+  marg: {
+    marginTop: theme.spacing(7),
+    marginLeft: theme.spacing(0),
+  },
+  itemDisplay: {
+     marginTop: theme.spacing(7),
+    // marginLeft: theme.spacing(25),
+  },
+  color: {
+    borderRadius: "30%",
+    width: "20px",
+    height: "20px",
+    display: "block",
+
+    "&:hover": {
+      backgroundColor: "yellow",
     },
   },
-}))
+}));
 
 const Cart = (props) => {
   const [cart, setCart] = useState([]);
+  const [hover, setHover] = useState(false);
   const classes = useStyles();
-  
+
   useEffect(() => {
     cartItems();
   }, []);
@@ -36,7 +58,7 @@ const Cart = (props) => {
     }
   };
   //doesn't decrement the item and instead removes the whole item
-  
+
   const decrementQuantity = async (id) => {
     try {
       const response = await CartService.postCart(id, -1);
@@ -50,7 +72,7 @@ const Cart = (props) => {
   const emptyCartItem = async () => {
     try {
       const response = CartService.emptyCart();
-       await response;
+      await response;
       cartItems();
       props.history.push("/");
     } catch (err) {
@@ -59,56 +81,110 @@ const Cart = (props) => {
   };
   console.log(cart);
   return (
-    <div className={classes.root}>
-      {!cart.items ? (
-        <Spinner animation="border" role="status">
-          <span className="sr-only">Loading...</span>
-        </Spinner>
-      ) : (
-        cart.items.map((item) => {
-          return (
-            <>
-              <div key={item._id} >
-                <h4>{item.productId.name}</h4>
-                <img
-                  src={item.productId.api_featured_image}
-                  style={{ width: "10rem" }}
-                  alt="api featured image"
-                />
-                <h6>{item.productId.name}</h6>
-                <p>{item.productId.category}</p>
-                <p>{item.productId.rating}</p>
-                <h4> {`£ ${item.price}`}</h4>
-                <button onClick={() => incrementQuantity(item.productId._id)}>
-                  +
-                </button>
-                {`you have ${item.quantity} ${item.productId.name} in total`}
-                <button onClick={() => decrementQuantity(item.productId._id)}>
-                  -
-                </button>
-                <h5>{`total price ${item.total}`}</h5>
+    <Box p="50px">
+      <Grid className={classes.root}>
+        <Box p="50px">
+          <Grid justify="space-between" container spacing={24}>
+            <h4>Product</h4>
+            <h4>Price</h4>
+            <h4>Qty</h4>
+            <h4>Total</h4>
+          </Grid>
 
-                <div>
-                  {/* {item.productId.product_colors.map((colors) => {
-                      return (
-                        <p style={{ color: colors.hex_value }}>
-                          {colors.hex_value}
+          <div>
+            {!cart.items ? (
+              <Spinner animation="border" role="status">
+                <span className="sr-only">Loading...</span>
+              </Spinner>
+            ) : (
+              cart.items.map((item) => {
+                return (
+                  <>
+                    <Grid
+                      justify="space-between"
+                      container
+                      spacing={1}
+                      className={classes.marg}
+                      // sm={4} xs={4}
+                    >
+                      <Grid item>
+                        <img
+                          src={item.productId.api_featured_image}
+                          style={{ width: "10rem" }}
+                          alt={item.productId.name}
+                        />
+
+                        <p style={{ wordWrap: "break-word" }}>
+                          {item.productId.name}
                         </p>
-                      );
-                    })} */}
-                </div>
-              </div>
-            </>
-          );
-        })
-      )}
-      <div>
-        {/* key={!product?"":product._id} */}
-        <h6>subTotal{cart.subTotal}</h6>
-        <button onClick={() => emptyCartItem()}>Empty cart</button>
-      </div>
-    </div>
+
+                        {item.productId.product_colors.map((colors) => {
+                          return (
+                            <p
+                              style={{
+                                backgroundColor: colors.hex_value,
+                                display: "inline-block",
+                                wordWrap: "break-word",
+                              }}
+                              className={classes.color}
+                            ></p>
+                          );
+                        })} 
+                      </Grid>
+
+                      <Grid item className={classes.itemDisplay}>
+                        <h5> {`£ ${item.price}`}</h5>
+                      </Grid>
+                      <Grid className={classes.itemDisplay} item>
+                        <Grid justify="space-between" container spacing={1}>
+                          <button
+                            onClick={() =>
+                              incrementQuantity(item.productId._id)
+                            }
+                          >
+                            +
+                          </button>
+                          <h5>{item.quantity}</h5>
+
+                          <div>
+                            <button
+                              onClick={() =>
+                                decrementQuantity(item.productId._id)
+                              }
+                            >
+                              -
+                            </button>
+                          </div>
+                        </Grid>
+                      </Grid>
+
+                      <Grid item className={classes.itemDisplay}>
+                        <h5>{`£${item.total}`}</h5>
+                      </Grid>
+                    </Grid>
+                  </>
+                );
+              })
+            )}
+            <div>
+              {/* key={!product?"":product._id} */}
+              <h6>subTotal{cart.subTotal}</h6>
+              <button onClick={() => emptyCartItem()}>Empty cart</button>
+            </div>
+          </div>
+        </Box>
+      </Grid>
+    </Box>
   );
 };
 
 export default Cart;
+
+{
+  /* <button onClick={() => decrementQuantity(item.productId._id)}>
+x
+</button> */
+}
+{
+  /* */
+}
---
+++ client/src/pages/CartGrid.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CartGrid = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CartGrid
